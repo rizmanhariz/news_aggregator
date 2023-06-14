c2cfc97ec025ee0e8ff05aa3d3a90449526afdaf
Rizman Hariz <rizmanhariz@gmail.com>
2023-06-14 20:48:47 +0800

    redundant file

--- helpers/query.helper.js
+++
@@ -1,9 +0,0 @@
-function getMongoPagination({ page, limit }) {
-  const finalPage = parseInt(page, 10) || 1;
-  const skip = (finalPage - 1) * (parseInt(limit, 10) || 10);
-  return { skip, page: finalPage};
-}
-
-module.exports = {
-  getMongoPagination,
-};
